51af8c4ba4262092d363c040b6a2a0905020915a
abdalkareemwanly <abd.alkareem.wanly.2023@gmail.com>
2023-10-15 14:07:24 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <h1 className="text-2xl">hello</h1>
+  </React.StrictMode>
+);
