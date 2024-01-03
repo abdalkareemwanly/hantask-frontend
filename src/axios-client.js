d1dcc7e3a9a9d71835c3803ba717da8440f69eef
abdalkareemwanly <abd.alkareem.wanly.2023@gmail.com>
2024-01-03 07:18:34 -0800

    first after few days

--- src/axios-client.js
+++ src/axios-client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `http://127.0.0.1:8000/api`,
+  baseURL: `https://api.hantask.at/api`,
 });
 
 axiosClient.interceptors.request.use((config) => {
