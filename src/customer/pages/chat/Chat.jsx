fa0bd128d4a29ad0f7bd8fd2578ef33fa7bfde3a
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2024-01-22 21:58:22 +0300

    make the message photo as the first letter of user name

--- src/customer/pages/chat/Chat.jsx
+++ src/customer/pages/chat/Chat.jsx
@@ -8,31 +8,44 @@ import CustomerChatHeader from "./components/ChatHeader";
 
 function CustomerChat(props) {
   try {
-    const getData = async () => {
-      const res = await axiosClient.get(`buyer/getContact`);
-      return res;
-    };
-
-    const { data: contacts, queryClient } = useQueryHook(["contacts"], () => getData());
-
-    useEffect(() => {
-      setTimeout(() => {
-        queryClient.prefetchQuery({
-          queryKey: ["contacts"],
-          queryFn: () => getData(),
-          staleTime: 60 * 60 * 1000,
-        });
-      }, 500);
-    }, [contacts, queryClient]);
+    const data = [
+      {
+        sender_id: 1,
+        reciever_id: 2,
+        message: <div>hello world</div>,
+        date: new Date().toLocaleString(),
+        file: "sdfk",
+      },
+      {
+        sender_id: 1,
+        reciever_id: 2,
+        message: <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sed eos ipsa neque nihil nemo!</strong>,
+        date: new Date().toLocaleString(),
+        file: "sdfk",
+      },
+      {
+        sender_id: 2,
+        reciever_id: 1,
+        message: <div>hello world</div>,
+        date: new Date().toLocaleString(),
+        file: "sdfk.pdf",
+      },
+      {
+        sender_id: 2,
+        reciever_id: 1,
+        message: <div>Lorem ipsum dolor sit amet, consectetusdf sdfsd fsdf sdfr adi</div>,
+        date: new Date().toLocaleString(),
+        file: "sdfk",
+      },
+    ];
 
     return (
       <>
         <Page>
-          {console.log(contacts)}
           <div className="recent-ticket xl:col-span-3 col-span-5 bg-blocks-color p-[20px] rounded-[10px]">
             <CustomerChatHeader reportId={1} orderId={3} reportFrom={"abdullah"} />
             <h4 className="text-[24px] leading-[1.2] font-[600] text-primary-text mb-[10px] mt-[20px]">All Conversation</h4>
-            <CustomerChatBody />
+            <CustomerChatBody messages={data} />
             <h4 className="text-[24px] leading-[1.2] font-[600] text-primary-text mb-[20px] mt-[20px]">Reply To Message</h4>
             <CustomerChatFooter />
           </div>
--- src/customer/pages/chat/components/ChatBody.jsx
+++ src/customer/pages/chat/components/ChatBody.jsx
@@ -1,42 +1,12 @@
 import CustomerChatMessage from "./ChatMessage";
 
-function CustomerChatBody(props) {
+function CustomerChatBody({ messages }) {
   try {
-    const data = [
-      {
-        sender_id: 1,
-        reciever_id: 2,
-        message: <div>hello world</div>,
-        date: new Date().toLocaleString(),
-        file: "sdfk",
-      },
-      {
-        sender_id: 1,
-        reciever_id: 2,
-        message: <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sed eos ipsa neque nihil nemo!</strong>,
-        date: new Date().toLocaleString(),
-        file: "sdfk",
-      },
-      {
-        sender_id: 2,
-        reciever_id: 1,
-        message: <div>hello world</div>,
-        date: new Date().toLocaleString(),
-        file: "sdfk",
-      },
-      {
-        sender_id: 2,
-        reciever_id: 1,
-        message: <div>Lorem ipsum dolor sit amet, consectetusdf sdfsd fsdf sdfr adi</div>,
-        date: new Date().toLocaleString(),
-        file: "sdfk",
-      },
-    ];
     return (
       <>
         <div className="flex flex-col">
-          {data.map((item, index) => {
-            return <CustomerChatMessage key={index} file={item.file} message={item.message} date={item.date} type={item.sender_id == 1 ? "sent" : "received"} isFirst={index == 0 || data[index].sender_id != data[index - 1].sender_id} name={"Abdullah"} image={"https://picsum.photos/200/300"} />;
+          {messages.map((item, index) => {
+            return <CustomerChatMessage key={index} file={item.file} message={item.message} date={item.date} type={item.sender_id == 1 ? "sent" : "received"} isFirst={index == 0 || messages[index].sender_id != messages[index - 1].sender_id} name={"Abdullah"} />;
           })}
         </div>
       </>
